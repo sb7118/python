c095608900b7a0757890c0b3b6a457bbd03df102
pixure <100357900+sb7118@users.noreply.github.com>
2022-08-22 22:09:04 +0545

    class

--- practice/Classes.py.py
+++ practice/Classes.py.py
@@ -6,9 +6,9 @@ class car:
 
     def cars(self):
         print(f'the name of car is {self.name} and its top speed is {self.topSpeed}')
-'''first make an object , then class name = values to the constructor'''
+#first make an object , then class name = values to the constructor
 car1 = car("lamborgini" , 200)
 car2 = car("bugatti" , 250)
-'''the first one to call will execute first'''
+#the first one to call will execute first
 car1.cars()
 car2.cars()
